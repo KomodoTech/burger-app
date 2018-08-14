d479253d470deacf34073ad04cafcb4337ec54f1
KomodoTech <alexandre.leibler@gmail.com>
2018-08-14 17:23:07 -0400

    structure the project to be a mern project. Going to drop this for now to continue the main tutorial and come back later

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const app = express();
+
